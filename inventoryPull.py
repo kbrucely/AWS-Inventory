0fc5244f359106d4ac3b9b900aee71465024e6a6
kevin ly <kevinly630@gmail.com>
2017-04-19 15:16:14 -0400

    modified to add name output as an option instead of IP

--- inventoryPull.py
+++ inventoryPull.py
@@ -11,26 +11,35 @@ instances = ec2.instances.filter(
 
 #declare a dictionary and start storing stuff by platform
 hostList = []
+hostNameList = []
 for instance in instances:
+    for tags in instance.tags:
+        if tags['Key'] == 'Name':
+            tag = tags['Value']
     hostList.append(instance.private_ip_address)
+    hostNameList.append(tag)
+
 
 linuxList = []
 windowsList = []
-nameList = []
-
-for instance in instances:
-    if instance.platform != 'windows':
-        linuxList.append(instance.private_ip_address)
-    else:
-        windowsList.append(instance.private_ip_address)
+linuxNameList = []
+windowsNameList = []
 
 for instance in instances:
     for tags in instance.tags:
         if tags['Key'] == 'Name':
-            nameList.append(tags['Value'])
+            tag = tags['Value']
+    if instance.platform != 'windows':
+        linuxList.append(instance.private_ip_address)
+        linuxNameList.append(tag)
+    else:
+        windowsList.append(instance.private_ip_address)
+        windowsNameList.append(tag)
+
 
 #add the hosts to the json output    
 output = dict(all=dict (hosts=hostList), linux=dict(hosts=linuxList), windows=dict(hosts=windowsList))
+nameOutput = dict(all=dict (hosts=hostNameList), linux=dict(hosts=linuxNameList), windows=dict(hosts=windowsNameList))
 
 if len(sys.argv) == 1:
     print ('please run with parameter --file for file output and --list for json output to console')
@@ -38,7 +47,7 @@ if len(sys.argv) == 1:
 
 
 #dump to file if user asks.  json and regular text
-if sys.argv[1]=='--file':
+if sys.argv[1]=='--IP':
     with open('jsonoutput.txt', 'w') as outfile:
         json.dump(output, outfile,indent=4)
 
@@ -53,9 +62,19 @@ if sys.argv[1]=='--file':
         for x in windowsList:
             outfile.write(x+'\n')  
 
-    with open('nameoutput.txt', 'w') as outfile:
+elif sys.argv[1]=='--name':
+    with open('jsonoutput.txt', 'w') as outfile:
+        json.dump(nameOutput, outfile,indent=4)
+
+    with open('regoutput.txt', 'w') as outfile:
         outfile.write('[all]'+'\n')
-        for x in nameList:
+        for x in hostNameList:
+            outfile.write(x+'\n')
+        outfile.write('\n'+'[linux]'+'\n')
+        for x in linuxNameList:
+            outfile.write(x+'\n')
+        outfile.write('\n'+'[windows]'+'\n')
+        for x in windowsNameList:
             outfile.write(x+'\n')
 
 elif sys.argv[1]=='--list':
@@ -67,4 +86,4 @@ elif sys.argv[1]=='--host':
     print (json.dumps(emptyDict,indent=4))
 
 else:
-    print ('please run with parameter --file for file output and --list for json output to console')
+    print ('please run with parameter --IP for ip output, --name for name output and --list for json output to console')
